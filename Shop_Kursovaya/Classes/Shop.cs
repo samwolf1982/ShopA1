dd41f295e340aeba1bbc9c1d41ffd22d54123933
Sam <biryukwolf@yahoo.com>
2015-05-16 23:13:02 +0300

    Добавлен пустой класс Shop

---
+++ Shop_Kursovaya/Classes/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop_Kursovaya.Classes
+{
+    /// <summary>
+    /// Класс для одиночного магазина. Возможно разширение для линии магазинов
+    /// </summary>
+    class Shop
+    {
+    }
+}
